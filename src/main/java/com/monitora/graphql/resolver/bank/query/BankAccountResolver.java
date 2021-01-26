cd0e5039215c378c7c244832ef2ec45955cf1a8e
marcelo.scarpim@monitoratec.com.br <marcelo.scarpim@monitoratec.com.br>
2021-01-26 15:25:01 -0300

    Inserido o método getSelectionSet na query de BankAccountResolver

--- src/main/java/com/monitora/graphql/resolver/bank/query/BankAccountResolver.java
+++ src/main/java/com/monitora/graphql/resolver/bank/query/BankAccountResolver.java
@@ -6,17 +6,31 @@ import com.monitora.graphql.domain.bank.Currency;
 import graphql.GraphQLException;
 import graphql.kickstart.spring.error.ThrowableGraphQLError;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.SelectedField;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class BankAccountResolver implements GraphQLQueryResolver {
-    public BankAccount bankAccount(UUID id) {
+    public BankAccount bankAccount(UUID id, DataFetchingEnvironment environment) {
+        
+        //pegar quais os campos o usuário selecionou na query
+        log.info("Fields in query selector {}", environment.getSelectionSet().getFields());
+
+        List<String> requestedField = environment.getSelectionSet().getFields().stream().map(SelectedField::getName).collect(Collectors.toUnmodifiableList());
+
+        log.info("Requested fields {}", requestedField);
+
+        if (environment.getSelectionSet().contains("client/firstName")) {
+            log.info("Client solicited the field 'firstName'");
+        }
 
 //        throw new RuntimeException("Cant connect to the postgresql on AWS: select f, a from bankAccount");
 //        throw new GraphQLException("Erro ao buscar dados do BankAccount");
