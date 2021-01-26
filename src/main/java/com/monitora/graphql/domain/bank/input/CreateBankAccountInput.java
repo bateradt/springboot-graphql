a6f2d4c6c9eb2de5f401496166bd4116ee62de77
marcelo.scarpim@monitoratec.com.br <marcelo.scarpim@monitoratec.com.br>
2021-01-26 16:54:18 -0300

    Aula16 - Custom Scalar Types

--- src/main/java/com/monitora/graphql/domain/bank/input/CreateBankAccountInput.java
+++ src/main/java/com/monitora/graphql/domain/bank/input/CreateBankAccountInput.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class CreateBankAccountInput {
     String agency;
     String number;
+    int age;
 }
--- src/main/java/com/monitora/graphql/resolver/bank/ClientResolver.java
+++ src/main/java/com/monitora/graphql/resolver/bank/ClientResolver.java
@@ -1,16 +1,13 @@
 package com.monitora.graphql.resolver.bank;
 
-import com.monitora.graphql.domain.bank.Asset;
 import com.monitora.graphql.domain.bank.BankAccount;
 import com.monitora.graphql.domain.bank.Client;
 import graphql.execution.DataFetcherResult;
 import graphql.kickstart.execution.error.GenericGraphQLError;
-import graphql.kickstart.tools.GraphQLQueryResolver;
 import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
@@ -23,23 +20,32 @@ public class ClientResolver implements GraphQLResolver<BankAccount> {
             Runtime.getRuntime().availableProcessors()
     );
 
-//    public DataFetcherResult<Client> client(BankAccount bankAccount) {
+//    public DataFetcherResult<Client> clienta(BankAccount bankAccount) {
 //        log.info("Request client data from bank account id {} ", bankAccount.getId());
 //
 //        return DataFetcherResult.<Client>newResult()
-//                .data(Client.builder().id(UUID.randomUUID()).firstName("Cliente novo").lastName("Da Silva").build()).build();
-////                .error(new GenericGraphQLError("Could not retrieve all data of client")).build();
+//                .data(Client.builder().id(UUID.randomUUID()).firstName("Cliente novo").lastName("Da Silva").build()).build()
+//               .error(new GenericGraphQLError("Could not retrieve all data of client")).build();
 //    }
 
     //irá executar uma nova thread para executar este serviço e ao finalizar irá retornar o valor
-    //semelhando ao Promise ou async/wait do javascript
+    //semelhante ao Promise ou async/wait do javascript
     public CompletableFuture<DataFetcherResult<Client>> client(BankAccount bankAccount) {
         return CompletableFuture.supplyAsync(
                 () -> {
                     log.info("Settings assets fro bank account id {}", bankAccount.getId());
 
+//                    var build = Client.builder().id(UUID.randomUUID())
+//                            .firstName("Cliente novo")
+//                            .lastName("Da Silva").build();
+//                    return build;
+
+
                     return DataFetcherResult.<Client>newResult()
-               .data(Client.builder().id(UUID.randomUUID()).firstName("Cliente novo").lastName("Da Silva").build()).build();
+                            .data(Client.builder().id(UUID.randomUUID())
+                            .firstName("Cliente novo")
+                            .lastName("Da Silva").build())
+                            .build();
 
                 },
                 executorService
